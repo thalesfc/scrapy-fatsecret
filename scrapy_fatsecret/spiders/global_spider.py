f9749929d8ae714aac49bf1bd6c793697ac60790
thalesfc <thalesfc@gmail.com>
2015-08-07 20:21:44 -0300

    First version of fat secret spider

--- scrapy_fatsecret/spiders/global_spider.py
+++ scrapy_fatsecret/spiders/global_spider.py
@@ -1,25 +1,33 @@
 # -*- coding: utf-8 -*-
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy_fatsecret.items import UserItem
 
 
 class GlobalSpiderSpider(CrawlSpider):
     name = 'global_spider'
     allowed_domains = ['fatsecret.com']
     start_urls = ['http://www.fatsecret.com/Default.aspx?pa=mem&u=jilly.beans']
+    # TODO change start url
 
     rules = [
         # Extract all links and follow links from them
         # (If "allow" is not given, it will match all links.)
-        Rule(LinkExtractor(), follow=True, callback='parse_item'),
+        # Rule(LinkExtractor(), follow=True),
+        # TODO uncomment this
+
+        # follow members page
+        Rule(
+            LinkExtractor(
+                allow=r'member/\S+',
+                ), follow=True, callback="parse_user")
+        # TODO change follow to true
     ]
 
-    def parse_item(self, response):
-        print 10 * '@', response.url
-        # i = ScrapyFatsecretItem()
-        # i['domain_id'] =
-        #       response.xpath('//input[@id="sid"]/@value').extract()
-        # i['name'] = response.xpath('//div[@id="name"]').extract()
-        # i['description'] =
-        #        response.xpath('//div[@id="description"]').extract()
-        # return i
+    def parse_user(self, response):
+        item = UserItem()
+        item['id'] = response.url.split('/')[-1]
+        item['name'] = response.xpath('//div[@class="NBBox"]/div/table/\
+                tr/td[2]/div/h1/text()').extract()
+        item['link'] = response.url
+        yield item
