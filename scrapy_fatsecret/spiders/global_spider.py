fd27fa9f4241338d1d429a87a0d52805907d1555
thalesfc <thalesfc@gmail.com>
2015-08-11 20:02:35 -0300

    Include process_value arg

--- scrapy_fatsecret/spiders/global_spider.py
+++ scrapy_fatsecret/spiders/global_spider.py
@@ -3,26 +3,69 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy_fatsecret.items import UserItem
 
+import re
+from urllib import unquote
 
-class GlobalSpiderSpider(CrawlSpider):
+f = open('log.txt', 'w')
+
+
+def process_value(value):
+    print >> f, "\n"
+    print >> f, value
+
+    # decode html percent chars, e.g., %3f --> /
+    # for some dark reasons, this function only works
+    # for first level urls, so if we have 2 urls
+    # embeed in one another, we have to double call it
+    count = 0
+    while "%" in value and count != 4:
+        count += 1
+        value = unquote(unquote(value))
+
+    if "%" in value:
+        return 'http://www.fatsecret.com'
+    print >> f, value
+
+    # 1st pre processing rule
+    # consider facebook redirect
+    rule = re.search("ReturnUrl=(.*)$", value)
+    if rule:
+        print >> f, "RULE REDIRECT", rule.group(1)
+        return process_value(rule.group(1))
+
+    # 2nd rule
+    # remove errors redirect
+    rule = re.search("ErrorLogUserFeedback", value)
+    if rule:
+        print >> f, "RULE ERROR", None
+        return None
+
+    print >> f, "Normal mode", value
+    return value
+
+
+class GlobalSpider(CrawlSpider):
     name = 'global_spider'
     allowed_domains = ['fatsecret.com']
-    start_urls = ['http://www.fatsecret.com/Default.aspx?pa=mem&u=jilly.beans']
+    start_urls = ['http://www.fatsecret.com/member/losinit1655']
     # TODO change start url
 
     rules = [
+        # 1st rule - members page
+        Rule(
+            LinkExtractor(
+                allow=r'^(http://www\.fatsecret\.com/member/[^\/\?]+)$',
+                process_value=process_value
+            ),
+            follow=False, callback="parse_user"
+        )
+
+        # 2nd rule - follow everything to get links
         # Extract all links and follow links from them
         # (If "allow" is not given, it will match all links.)
         # Rule(LinkExtractor(), follow=True),
         # TODO uncomment this
-
-        # follow members page
-        Rule(
-            LinkExtractor(
-                allow=r'member/[^\/\?]+$',
-                ), follow=True, callback="parse_user")
-        # TODO change follow to true
-    ]
+    ]  # end of rules
 
     def parse_user(self, response):
         item = UserItem()
