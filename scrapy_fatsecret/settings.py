abbef66d943726998917dd936842cbbf501f75e7
thalesfc <thalesfc@gmail.com>
2015-08-18 18:23:59 -0300

    Include settings

---
+++ scrapy_fatsecret/settings.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for scrapy_fatsecret project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'scrapy_fatsecret'
+
+SPIDER_MODULES = ['scrapy_fatsecret.spiders']
+NEWSPIDER_MODULE = 'scrapy_fatsecret.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the
+# user-agent
+# USER_AGENT = 'scrapy_fatsecret (+http://www.yourdomain.com)'
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+# CONCURRENT_REQUESTS=32
+
+# Log configurations
+# LOG_FILE = 'log.txt'
+LOG_LEVEL = 'DEBUG'
+
+# Configure a delay for requests for the same website (default: 0)
+# See http://scrapy.readthedocs.org/en/latest/topics/settings
+#   .html#download-delay
+# See also autothrottle settings and docs
+DOWNLOAD_DELAY = 5
+RANDOMIZE_DOWNLOAD_DELAY = True
+# The download delay setting will honor only one of:
+CONCURRENT_REQUESTS_PER_DOMAIN = 4
+# CONCURRENT_REQUESTS_PER_IP=16
+
+# Disable cookies (enabled by default)
+# COOKIES_ENABLED=False
+
+# Disable Telnet Console (enabled by default)
+# TELNETCONSOLE_ENABLED=False
+
+# Override the default request headers:
+# DEFAULT_REQUEST_HEADERS = {
+# 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+# }
+
+# Enable or disable spider middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+# SPIDER_MIDDLEWARES = {
+#    'scrapy_fatsecret.middlewares.MyCustomSpiderMiddleware': 543,
+# }
+
+# Enable or disable downloader middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+# DOWNLOADER_MIDDLEWARES = {
+#    'scrapy_fatsecret.middlewares.MyCustomDownloaderMiddleware': 543,
+# }
+
+# Enable or disable extensions
+# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
+# EXTENSIONS = {
+#    'scrapy.telnet.TelnetConsole': None,
+# }
+
+# Configure item pipelines
+# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+    'scrapy_fatsecret.pipelines.ValidDataPipeLine': 300,
+    'scrapy_fatsecret.pipelines.JsonWriterPipeline': 400,
+}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
+# NOTE: AutoThrottle will honour the standard settings for concurrency
+# and delay
+# AUTOTHROTTLE_ENABLED=True
+# The initial download delay
+# AUTOTHROTTLE_START_DELAY=5
+# The maximum download delay to be set in case of high latencies
+# AUTOTHROTTLE_MAX_DELAY=60
+# Enable showing throttling stats for every response received:
+# AUTOTHROTTLE_DEBUG=False
+
+# Enable and configure HTTP caching (disabled by default)
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware
+# .html#httpcache-middleware-settings
+# HTTPCACHE_ENABLED=True
+# HTTPCACHE_EXPIRATION_SECS=0
+# HTTPCACHE_DIR='httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES=[]
+# HTTPCACHE_STORAGE='scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# my definitions
+# does not allow 302 redirect
+# REDIRECT_ENABLED = False
+
+import items
+
+ITEM_SETTINGS = {
+    items.UserItem: {
+        'FILENAME': 'user.json',
+        'FIELD_VALIDATION': ['name', 'id', 'link'],
+        'FIELD_AS_LIST': []
+    },
+    items.PostItem: {
+        'FILENAME': 'post.json',
+        'FIELD_VALIDATION': ['id', 'user_id', 'date'],
+        'FIELD_AS_LIST': ['comments']
+    }
+}
