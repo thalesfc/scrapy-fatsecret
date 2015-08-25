4c2ea8028e8256149a86b6d5b49e3b28fa9c5522
thalesfc <thalesfc@gmail.com>
2015-08-25 15:37:41 -0300

    Merge login spider (buddies) with global spider, which now can log

--- scrapy_fatsecret/spiders/global_spider.py
+++ scrapy_fatsecret/spiders/global_spider.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-from scrapy_fatsecret.helpers import users, posts
+from scrapy_fatsecret.helpers import users, posts, buddies
+from scrapy import FormRequest, Request
+import config
+import logging
 
 
 class GlobalSpider(CrawlSpider):
     name = 'global_spider'
     allowed_domains = ['fatsecret.com']
+    login_url = 'https://www.fatsecret.com/Auth.aspx?pa=s'
     start_urls = ['http://www.fatsecret.com/member/dorindam59']
-    # TODO change start url
 
     rules = [
         # 1st rule - members page
@@ -26,5 +29,34 @@ class GlobalSpider(CrawlSpider):
             ),
             callback=posts.schedule_posts,
             follow=True
+        ),
+
+        # 3rd rule [LOGGED] - buddies: scrapy users' friends
+        Rule(
+            LinkExtractor(allow='pa=memb', deny='(order=|pg=0)'),
+            follow=True,
+            callback=buddies.parse_buddy
         )
     ]  # end of rules
+
+    # start request is used to process files instead of start_urls
+    # we are using it to log our spider on the website
+    def start_requests(self):
+        return [
+            FormRequest(
+                self.login_url,
+                formdata={
+                    'ctl00$ctl07$Logincontrol1$Name': config.GMAIL_USER,
+                    'ctl00$ctl07$Logincontrol1$Password': config.PASSWORD,
+                    '__EVENTTARGET': 'ctl00$ctl07$Logincontrol1$Login',
+                },
+                callback=self.after_login
+            )
+        ]
+
+    def after_login(self, response):
+        if response.url == 'https://www.fatsecret.com/Default.aspx?pa=m':
+            self.log('Successfully LOGGED.', logging.INFO)
+            return [Request(url=u) for u in self.start_urls]
+        else:
+            self.log('LOGIN error: ' + response.url, logging.CRITICAL)
